c91cec912abd53a675564f81cb1f66a5c484b448
Jose Mejia <aalejandro.mreyes@icloud.com>
2019-06-09 20:03:14 -0400

    Gets MVP Done

---
+++ athlete/src/com/lambdaschool/solution/BaseballAthleteCreationImp.java
@@ -0,0 +1,10 @@
+package com.lambdaschool.solution;
+
+public class BaseballAthleteCreationImp implements AthleteService
+{
+    @Override
+    public void displayAthlete()
+    {
+        System.out.println("Baseball Athlete");
+    }
+}
--- athlete/src/com/lambdaschool/solution/Main.java
+++ athlete/src/com/lambdaschool/solution/Main.java
@@ -20,17 +20,16 @@ public class Main
         injector = new HockeyAthleteCreationInjector();
         app = injector.getProcess();
         app.displayAthlete();
-        //
-        //
-        //        // Create a Rugby Athlete
-        //        injector = new RugbyAthleteCreationInjector();
-        //        app = injector.getProcess();
-        //        app.displayAthlete();
-        //
-        //        // Create a Baseball Athlete
-        //        injector = new BaseballAthleteCreationInjector();
-        //        app = injector.getProcess();
-        //        app.displayAthlete();
 
+
+        // Create a Rugby Athlete
+        injector = new RugbyAthleteCreationInjector();
+        app = injector.getProcess();
+        app.displayAthlete();
+
+        // Create a Baseball Athlete
+        injector = new BaseballAthleteCreationInjector();
+        app = injector.getProcess();
+        app.displayAthlete();
     }
 }
